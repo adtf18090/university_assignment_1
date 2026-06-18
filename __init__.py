fe92cc3133189f94bc723eac89438984b1305712
adtf18090 <adtf18090@gmail.com>
2026-06-18 17:03:51 +0900

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Stock Dashboard Package
-"""
-from .gui import StockDashboard
-from .fetcher_yfinance import YFinanceFetcher
-from .utils import search_stock_ticker
-
-__all__ = ["StockDashboard", "YFinanceFetcher", "search_stock_ticker"]
